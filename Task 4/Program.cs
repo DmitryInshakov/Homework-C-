ed7cdc13aeb4a9cc813b5e8701630cc29ad177e8
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-02-27 23:20:02 +0300

    Добавили задачу 4

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
+
+
