11dc3b085cac88f959679cbac1a8ed91b2cf4082
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-02-27 23:14:05 +0300

    Добавили задачу 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -5,9 +5,11 @@ int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
+    Console.Write("max = ");
     Console.WriteLine(a);
 }
 else
 {
+    Console.Write("max = ");
     Console.WriteLine(b);  
 }
