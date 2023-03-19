780eab99d8ef762fb8780dfc782aa38e7664c0de
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-03-19 11:20:09 +0300

    Добавили задачу 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int FindThirdRang (int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool CheckThirdDigit (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (CheckThirdDigit(number))
+{
+    Console.WriteLine(FindThirdRang(number));
+}
