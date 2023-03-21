22c28571f7251f9c085be3a991284202d8561721
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-03-22 01:47:57 +0300

    Добавили задачу 23

---
+++ Task 23/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+bool CheckNaturalNumber(int number_natural)
+{
+    if (number_natural > 0)
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели число меньше единицы");
+        return false;
+    }
+}
+
+int TableCubes(int initial_number)
+{
+    Console.Write($"{initial_number} -> 1"); /* Число один в кубе всегда единица, делаем данную запись для возможности вывода полученных данных в цикле в строчку 
+    (если так не сделать,то в конце последнего числа всегда будет запятая)*/
+    for (int i = 2; i <= initial_number; i++)
+    {
+        Console.Write(", {0}",Math.Pow(i,3));
+    }
+    return 0;
+}
+
+Console.Write ("Введите число: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+if (CheckNaturalNumber(quantity) == true)
+{
+    TableCubes(quantity);
+}
+
