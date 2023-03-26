c74f1ba1311dc447d11e88c6f4ad7974fe1883cc
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-03-27 02:32:01 +0300

    Добавили задачу 29

--- Task 29/Program.cs
+++ Task 29/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void CreateArray (int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next (1,100);
+    }
+}
+
+void PrintArray (int [] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length - 1; i++)
+    {
+        Console.Write($"{mas[i]}, ");
+    }
+    Console.Write($"{mas[mas.Length-1]}");
+    Console.Write("]");
+}
+
+int [] array = new int [8];
+CreateArray(array);
+PrintArray(array);
+
+
+
+
