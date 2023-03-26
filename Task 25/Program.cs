d60a45796b2b1ed7a15fa30bdc16c46c5efac722
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-03-27 02:06:58 +0300

    Добавили задачу 25

---
+++ Task 25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Construction (int basis, int degrees)
+{
+    if (degrees == 0) return 1;
+    else return Construction(basis,degrees- 1) * basis;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (a==0 && n == 0)
+{
+    Console.WriteLine("Выражение не имеет смысла");
+}
+else 
+{   
+    if (n > 0)
+    {
+        Console.WriteLine(Construction(a,n));
+    }
+}
