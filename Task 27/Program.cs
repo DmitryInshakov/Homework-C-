ea4a8346eb7cd9672688fe7a7196a5ac2eec9cf4
Inshakov Dmitriy <inshakov.dima.68.pugna@mail.ru>
2023-03-27 02:16:18 +0300

    Добавили задачу 27

--- Task 27/Program.cs
+++ Task 27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int SumAllNumber (int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    Console.WriteLine($"{number} -> {SumAllNumber(number)}");
+}
+else
+{
+    Console.WriteLine("Введите положительное число");
+}
+
